650f9f7d5f60f5b20db1589dd589dabd62767db5
Saeed <appsaeed7@gmail.com>
2023-06-19 18:58:37 +0600

    video call experiment  app

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-const express = require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
-const ports = process.env.PORT || 3000
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const port = process.env.PORT || 8000;
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static(__dirname + "/public"));
+let clients = 0;
 
-app.get('/', (req, res) => {
-  res.redirect(`/${uuidV4()}`)
-})
-
-app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
-})
-
-io.on('connection', socket => {
-  socket.on('join-room', (roomId, userId) => {
-    socket.join(roomId)
-    socket.to(roomId).emit('user-connected', userId)
-    // This line here
-
-    socket.on('disconnect', () => {
-      socket.to(roomId).emit('user-disconnected', userId)
-    })
-  })
-})
-
- //Peer server
-
-const { PeerServer } = require('peer');
-const peerServer = PeerServer({ 
-    port: 443, 
-    path: '/' ,                   
+io.on("connection", function (socket) {
+  socket.on("NewClient", function () {
+    if (clients < 2) {
+      if (clients == 1) {
+        this.emit("CreatePeer");
+      }
+    } else this.emit("SessionActive");
+    clients++;
+  });
+  socket.on("Offer", SendOffer);
+  socket.on("Answer", SendAnswer);
+  socket.on("disconnect", Disconnect);
 });
 
+function Disconnect() {
+  if (clients > 0) {
+    if (clients <= 2) this.broadcast.emit("Disconnect");
+    clients--;
+  }
+}
 
+function SendOffer(offer) {
+  this.broadcast.emit("BackOffer", offer);
+}
 
-server.listen(ports)
+function SendAnswer(data) {
+  this.broadcast.emit("BackAnswer", data);
+}
+
+http.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
